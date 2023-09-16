7da1d3988309c56a632bf89bf2b830a27e29c4a1
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-17 00:30:12 +0200

    add gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 mkdir ~/.ssh
 echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
-ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" > ~/.ssh/known_hosts && chmod 700 ~/.ssh/known_hosts
+ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" > ~/.ssh/known_hosts && chmod 777 ~/.ssh/known_hosts
+
+whoami
 
 ssh "$INPUT_USERNAME"@"$INPUT_HOST" -p "$INPUT_PORT" touch testfile_from_today.txt
