2ff46c44b0a16db5f140cd77adf4f68199d11c25
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-16 23:17:26 +0200

    add gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
 
 
-ssh "@INPUT_USERNAME@$INPUT_HOST" -p "$INPUT_PORT" touch testfile_from_today.txt
+ssh "$INPUT_USERNAME@$INPUT_HOST" -p "$INPUT_PORT" touch testfile_from_today.txt
