89f976b3bdfa6ba2a24400df0df2caba34e5d45b
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-16 18:47:00 +0200

    add ssh connection parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+list=$(ls -lah)
+echo "list=$list" >> $GITHUB_OUTPUT
+
+whereami=$(pwd)
+echo "whereami=$whereami" >> $GITHUB_OUTPUT
