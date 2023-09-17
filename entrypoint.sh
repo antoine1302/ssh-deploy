4ff9053513dee64d6c807e1d28758a97f7cc0b71
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-17 02:09:08 +0200

    add gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,5 @@
 mkdir ~/.ssh
 echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
 
-eval "$(ssh-agent -s)"
-bash -c 'ssh-add <(echo "$INPUT_KEY")'
 ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" > ~/.ssh/known_hosts && chmod 700 ~/.ssh/known_hosts
-ssh "$INPUT_USERNAME"@"$INPUT_HOST" -p "$INPUT_PORT"
+ssh "$INPUT_USERNAME@$INPUT_HOST" -p "$INPUT_PORT"
