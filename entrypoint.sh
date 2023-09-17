26e1a362ec19a56bc5b54b25465ce1b3b460a4c1
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-17 20:00:40 +0200

    add gitignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir ~/.ssh
 echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
 ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" >> /etc/ssh/ssh_known_hosts
 current_date=current_date_time=$(date +"%Y-%m-%d")
-rsync -avzh -e "ssh -p $INPUT_PORT" ./ "$INPUT_USERNAME@$INPUT_HOST:~/projects/$INPUT_REPOSITORY/$current_date/"
+rsync -avzh -e "ssh -i ~/.ssh/id_rsa -p $INPUT_PORT" ./ "$INPUT_USERNAME@$INPUT_HOST:~/projects/$INPUT_REPOSITORY/$current_date/"
