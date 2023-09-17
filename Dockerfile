6a25037a3ca37c62265436c6c506994e618e7c6c
Totoro1302 <garichard85@gmail.com>
2023-09-17 22:23:17 +0200

    Add deployment logic (#5)

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,9 @@ FROM alpine:3.18
 
 COPY entrypoint.sh /entrypoint.sh
 
+RUN apk update && \
+    apk add --no-cache bash openssh-client rsync bash
+
+SHELL ["bin/bash"]
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/sh -l
+#!/bin/bash
 
-list=$(ls -lah)
-echo "list=$list" >> $GITHUB_OUTPUT
-
-whereami=$(pwd)
-echo "whereami=$whereami" >> $GITHUB_OUTPUT
+mkdir ~/.ssh
+echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
+ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" >> /etc/ssh/ssh_known_hosts
+path_on_remote="/home/$INPUT_USERNAME/deployments/$INPUT_REPOSITORY/$(date +"%Y-%m-%d")/$(date +"%Y%m%d%H%M%S")"
+ssh -i /github/home/.ssh/id_rsa "$INPUT_USERNAME@$INPUT_HOST" -p "$INPUT_PORT" mkdir -p "$path_on_remote"
+rsync -avzh --exclude ".git" --exclude ".github" -e "ssh -i /github/home/.ssh/id_rsa -p $INPUT_PORT" ./ "$INPUT_USERNAME@$INPUT_HOST:$path_on_remote/"
+ssh -i /github/home/.ssh/id_rsa "$INPUT_USERNAME@$INPUT_HOST" -p "$INPUT_PORT" ln -fsn "$path_on_remote" "/home/$INPUT_USERNAME/projects/$INPUT_REPOSITORY"
