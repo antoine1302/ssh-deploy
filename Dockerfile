1bd27d342f2bdd030d0d2bf15fae6d9562ed5a1b
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-09-17 21:01:12 +0200

    add gitignore

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM alpine:3.18
 COPY entrypoint.sh /entrypoint.sh
 
 RUN apk update && \
-    apk add --no-cache bash openssh-client rsync
+    apk add --no-cache bash openssh-client rsync bash
+
+SHELL ["bin/bash"]
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir ~/.ssh
 echo "$INPUT_KEY" > ~/.ssh/id_rsa && chmod 700 ~/.ssh/id_rsa
 ssh-keyscan -p "$INPUT_PORT" -H "$INPUT_HOST" >> /etc/ssh/ssh_known_hosts
-current_date=current_date_time=$(date +"%Y-%m-%d")
-rsync -avzh -e "ssh -i /github/home/.ssh/id_rsa -p $INPUT_PORT" ./ "$INPUT_USERNAME@$INPUT_HOST:~/projects/$INPUT_REPOSITORY/$current_date/"
+current_date_time=$(date +"%Y-%m-%d")
+rsync -avzh -e "ssh -i /github/home/.ssh/id_rsa -p $INPUT_PORT" ./ "$INPUT_USERNAME@$INPUT_HOST:~/projects/$INPUT_REPOSITORY/$current_date_time/"
